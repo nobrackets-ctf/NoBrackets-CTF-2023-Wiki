d426fc7a98aeb12dade3a07d41f28c099a089b1d
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-09-18 11:36:34 +0200

    sdfgksdfg

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://wiki.nobrackets.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/NoBrackets-CTF-2023-Wiki/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
