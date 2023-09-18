a97ababda5e9737a012997012dbb06266b35d417
FreezingKas <57356272+FreezingKas@users.noreply.github.com>
2023-09-18 11:37:04 +0200

    FUCK ME

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://wiki.nobrackets.fr',
+  url: 'https://nobrackets-ctf.nobrackets.fr',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/NoBrackets-CTF-2023-Wiki/',
