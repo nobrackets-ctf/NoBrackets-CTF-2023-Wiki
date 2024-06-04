fdf43a6dce73862240e064a7e67aac9b3844849b
Drahoxx <Drahoxx@users.noreply.github.com>
2024-06-04 11:11:13 +0200

    Fix migration from wiki.nobrackets.fr to nobrackets.fr

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="https://wiki.nobrackets.fr/ctf">
+            to="/ctf">
             Plus d'infos 🚩
           </Link>
         </div>
